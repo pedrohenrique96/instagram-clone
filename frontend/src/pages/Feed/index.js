d71f59eb36f4cb108645ae5d3f9f9c3447550bcf
Pedro Henrique <pedrook16@gmail.com>
2019-10-04 06:32:49 -0300

    add

--- frontend/src/pages/Feed/index.js
+++ frontend/src/pages/Feed/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import GlobalStyles from "../../styles/global";
 
 import Header from "../../components/Headers";
-import Content from "../../components/Content";
+import Content from "../../components/ContentFeed";
 
 export default function Feed() {
   return (
